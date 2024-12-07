ebb36020d5f44bf48bcc3b331710b7033b933a4c
Sarathhub007 <ilamsarathchandra@gmail.com>
2024-12-08 01:48:58 +0530

    Updated Vite config for deployment

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: process.env.PORT || 5173,  
-    host: '0.0.0.0'  
+    port: process.env.PORT || 10000,  
+    host: '0.0.0.0',  
+    strictPort: true 
   }
 })
